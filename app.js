674b2ac610bb12ea5647612dc9826adf6365c878
Marion Jung <marionjung34@hotmail.com>
2023-08-03 16:40:56 +0100

    port updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Import the required modules and set up the express app
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set the view engine to EJS and use the bodyParser middleware
 app.set('view engine', 'ejs');
@@ -19,7 +19,7 @@ app.use('/result', turnRouter);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app; 
