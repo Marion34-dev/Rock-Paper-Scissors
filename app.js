4f6e7dd11b4c61199e4b56db3f6d2c1b87c5b810
Marion Jung <marionjung34@hotmail.com>
2023-08-04 14:47:25 +0100

    Port updated

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-// Import the required modules and set up the express app
 const express = require('express');
+// const port = 3000;
+const host = process.env.HOST;
+const port = process.env.PORT;
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Set the view engine to EJS and use the bodyParser middleware
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-// Import the routers
 const indexRouter = require('./routes/index.js');
 const gameRouter = require('./routes/game.js');
 const turnRouter = require('./routes/result.js');
 
-// Use the indexRouter first, so the index page is shown first
 app.use('/', indexRouter);
 app.use('/game', gameRouter);
 app.use('/result', turnRouter);
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, host, () => {
+    // console.log(`Example app listening on http://localhost:${port}`);
+    console.log(`Connecting to ${host} ${port}`);
+
 });
 
 module.exports = app; 
